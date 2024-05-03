f68bd4a8eace2acace481f6e41fc47d23da47b10
ICodeForCoffee <poul102@gmail.com>
2024-05-03 11:03:44 -0400

    Add project files.

---
+++ WebsiteMonitor/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+/*
+
+
+
+Create a thread that sleeps for X seconds
+when the thread fires, run a playwright test
+
+update the console
+
+ * 
+ * 
+ */
+
+NewMethod();
+
+static void NewMethod()
+{
+    static async Task ThreadTest()
+    {
+
+    }
+}
