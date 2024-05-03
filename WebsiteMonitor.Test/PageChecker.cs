62ddd2b94183198033afe7b90bb08db58ce07323
ICodeForCoffee <poul102@gmail.com>
2024-05-03 11:04:36 -0400

    Making a simple change.

--- WebsiteMonitor.Test/PageChecker.cs
+++ WebsiteMonitor.Test/PageChecker.cs
@@ -1,7 +1,7 @@
 namespace WebsiteMonitor.Test
 {
     [TestClass]
-    public class UnitTest1 : PageTest
+    public class PageChecker : PageTest
     {
         [TestMethod]
         public async Task HomepageHasPlaywrightInTitleAndGetStartedLinkLinkingtoTheIntroPage()
